e837cbee6163bdbff1f69c199cd61498151663a0
Andesson Reis <andessonreys@gmail.com>
2025-10-22 23:19:20 -0300

    first commit

---
+++ src/main/java/io/andesson/dataflowmanager/DataflowmanagerApplication.java
@@ -0,0 +1,13 @@
+package io.andesson.dataflowmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataflowmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataflowmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/andesson/dataflowmanager/DataflowmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.andesson.dataflowmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataflowmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
